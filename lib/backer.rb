d6cafc243c4cac4c5393cda72016d14debe25927
Amanda Contreras <amandaben21@aol.com>
2020-03-14 16:28:34 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,19 +1,14 @@
 class Backer
   attr_reader :name, :backed_projects
 
-
   def initialize(name)
     @name = name
     @backed_projects = []
   end
 
-  def backed_projects
-    pbs = ProjectBacker.all.select {|pb| pb.backer == self}
-    pbs.map {|pb| pb.project}
-  end
-
-  def backed_project(project)
-    ProjectBacker.new(project, self)
+  def back_project(project)
+    backed_projects << project
+    project.backers << self
   end
 
 end
