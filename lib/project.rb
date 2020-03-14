9e36fb1a1e56b10845bc6e2a0a2d140b51b952d5
Amanda Contreras <amandaben21@aol.com>
2020-03-14 16:15:19 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,17 +1,17 @@
-def create_project_hash
-  html = File.read('fixtures/kickstarter.html')
-  kickstarter = Nokogiri::HTML(html)
+class Project
+  attr_reader :title, :backers
 
-  projects = {}
-
-  kickstarter.css("li.project.grid_4").each do |project|
-    title = project.css("h2.bbcard_name strong a").text
-    projects[title.to_sym] = {
-      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
-      :description => project.css("p.bbcard_blurb").text,
-      :location => project.css("ul.project-meta span.location-name").text,
-      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-    }
+  def initialize(title)
+    @title = title
   end
-  projects
+
+  def backers
+    pbs = ProjectBacker.all.select {|pb| pb.project == self}
+    pbs.map {|pb| pb.backer}
+  end
+
+  def add_backer(backer)
+    ProjectBacker.new(self, backer)
+  end
+
 end
